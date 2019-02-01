ee5a75448bb731a57acf471f84046336853ce285
Sarah-JJ <sarah_thekazekage@yahoo.com>
2019-02-01 14:42:28 +0300

    removed test jibberish remnants

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.post('/pharmacies', (req, res) => {
 
 app.post('/pharmacies/edit', (req, res) => {
 
-    PharmacyModel.updateOne({ _id: req.body.id }, { $set: { registered: req.body.registered, name: req.body.name }}, function(result, err){
+    PharmacyModel.updateOne({ _id: req.body.id }, { $set: { registered: req.body.registered}}, function(result, err){
         if(err) {
             console.log(err);
             res.json({"err": err})
